24a7fb1e7dbfbf979120cf1221f547e762423c67
DavidLopezSerrano <davidlopezs.sanjosemlg@fundacionloyola.net>
2026-02-19 16:40:13 +0100

    Primer push, creacion clases vacias - David

---
+++ AgendaColavorativa/src/modelo/Agenda.java
@@ -0,0 +1,25 @@
+package modelo;
+
+public class Agenda {
+
+	public Agenda() {
+
+	}
+
+	public void agregarContacto(Contacto c) {
+
+	}
+
+	public void eliminarContacto(String nombre) {
+
+	}
+
+	public String buscarContacto(String nombre) {
+
+		return nombre;
+	}
+
+	public void listarContactos() {
+
+	}
+}
