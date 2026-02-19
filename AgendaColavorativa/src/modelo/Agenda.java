137d491636d5da4b070a4d1b0d21f9fa1c38801a
DavidLopezSerrano <davidlopezs.sanjosemlg@fundacionloyola.net>
2026-02-19 16:44:39 +0100

    Creacion metodo agregar contacto - David

--- AgendaColavorativa/src/modelo/Agenda.java
+++ AgendaColavorativa/src/modelo/Agenda.java
@@ -1,13 +1,22 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Agenda {
+	
+	private ArrayList<Contacto> contactos = new ArrayList<>();
 
 	public Agenda() {
 
+		this.contactos=contactos;
+		
 	}
 
 	public void agregarContacto(Contacto c) {
 
+		contactos.add(c);
+		
+		
 	}
 
 	public void eliminarContacto(String nombre) {
