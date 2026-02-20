c6a7630a5e6c82c20cd5af39463829df040110a7
evamariabeltranrsanjosemlg-hub <evamariabeltranr.sanjosemlg@fundacionloyola.net>
2026-02-20 12:53:27 +0100

    Push desde rama-buscar-eliminar con metodos ej3

---
+++ AgendaColavorativa/src/controlador/Main.java
@@ -0,0 +1,13 @@
+package controlador;
+
+import modelo.*;
+
+public class Main {
+	
+	private static Agenda agenda = new Agenda();
+public static void main(String[] args) {
+	
+	Agenda.eliminarContacto("pepe");
+
+}
+}
--- AgendaColavorativa/src/modelo/Agenda.java
+++ AgendaColavorativa/src/modelo/Agenda.java
@@ -1,25 +1,52 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Agenda {
+
+	private static ArrayList<Contacto> contactos = new ArrayList<>();
 
 	public Agenda() {
 
+		this.contactos=contactos;
+		
 	}
 
 	public void agregarContacto(Contacto c) {
 
 	}
 
-	public void eliminarContacto(String nombre) {
-
+	public static void eliminarContacto(String nombre) {
+	    for (int i = 0; i < contactos.size(); i++) {
+	        if (contactos.get(i).getName().equalsIgnoreCase(nombre)) {
+	            contactos.remove(i);
+	        } else {
+	        	System.out.println("Este contacto no existe");
+	        }
+	    }
 	}
 
 	public String buscarContacto(String nombre) {
-
-		return nombre;
-	}
+        // Recorremos la lista buscando el nombre
+        for (Contacto c : contactos) {
+            if (c.getName().equalsIgnoreCase(nombre)) {
+                return c.toString(); 
+            }
+        }
+        return "Contacto no encontrado";
+    }
 
 	public void listarContactos() {
 
+		
 	}
+
+	public ArrayList<Contacto> getContactos() {
+		return contactos;
+	}
+
+	public void setContactos(ArrayList<Contacto> contactos) {
+		this.contactos = contactos;
+	}
+	
 }
